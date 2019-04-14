292ce974abd09109564450d59299fcf824e39d14
diazdaiz <40154072+diazdaiz@users.noreply.github.com>
2019-04-14 13:47:00 +0700

    Add files via upload

---
+++ week_4/naik_angkot.js
@@ -0,0 +1,30 @@
+function naikAngkot(arrPenumpang) {
+    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+    var i,j,mulai,akhir,output,bayar;
+    output=[];
+    for(i=0;i<arrPenumpang.length;i++){
+        for(j=0;j<rute.length;j++){
+            if(arrPenumpang[i][1]==rute[j]){
+                mulai=j;
+            }
+            if(arrPenumpang[i][2]==rute[j]){
+                akhir=j;
+            }
+        }
+        bayar=(akhir-mulai)*2000;
+        output.push({
+            "penumpang":arrPenumpang[i][0],
+            "naikDari":arrPenumpang[i][1],
+            "tujuan":arrPenumpang[i][2],
+            "bayar":bayar
+        })
+    }
+    return output;
+}
+
+//TEST CASE
+console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+// [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+//   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+
+console.log(naikAngkot([])); //[]
